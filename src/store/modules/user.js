a87132f45a8aa8f27450cb28fd421afcf0758bdd
moxianyou <1872499195@qq.com>
2022-09-27 10:44:44 +0800

    feat:token梳理

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -5,24 +5,41 @@ export default {
   namespaced: true,
   state: {
     token: null,
-    userInfo: {}
+    userInfo: {},
+    hrsaasTime: 0
   },
+
   mutations: {
+    // 修改token
     SET_TOKEN(state, token) {
       state.token = token
     },
+    // 修改用户信息
     SET_USER_INFO(state, userInfo) {
       state.userInfo = userInfo
     },
+    // 修改时间
+    SET_HRSAASTIME(state, hrsaasTime) {
+      state.hrsaasTime = hrsaasTime
+    },
+    // 移除用户信息
     REMOVE_USER_INFO(state) {
       state.userInfo = {}
+    },
+    // 移除token
+    REMOVE_TOKEN(state) {
+      state.token = null
     }
   },
+
   actions: {
+    // 登录
     async loginAction({ commit }, loginData) {
       const data = await login(loginData) // 在响应拦截器中已经处理过后端返回来的结果了
       commit('SET_TOKEN', data)
+      commit('SET_HRSAASTIME', +new Date())
     },
+    // 获取用户信息
     async getUserInfo({ commit }) {
       // 请求接口
       const res = await getUserInfo()
@@ -30,6 +47,11 @@ export default {
       const result = { ...res, ...res1 }
       commit('SET_USER_INFO', result)
       return JSON.parse(JSON.stringify(result)) // 后面会用到
+    },
+    // 退出
+    logout({ commit }) {
+      commit('REMOVE_USER_INFO')
+      commit('REMOVE_TOKEN')
     }
   }
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,17 @@
-
 import axios from 'axios'
 import { Message } from 'element-ui'
 import store from '@/store'
+import router from '@/router'
 
+const TimeOut = 3600
+// 对比时间是否超时
+const isCheckTimeout = () => {
+  const currentTime = Date.now()
+  const timeStamp = (currentTime - store.getters.hrsaasTime) / 1000
+  return timeStamp > TimeOut
+}
+
+// 创建axios实例
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // 基准地址
   timeout: 5000
@@ -12,6 +21,11 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     if (store.getters.token) {
+      if (isCheckTimeout()) {
+        store.dispatch('user/logout')
+        router.push('/login')
+        return Promise.reject(new Error('token超时'))
+      }
       config.headers.Authorization = `Bearer ${store.getters.token}`
     }
     return config
@@ -38,7 +52,15 @@ service.interceptors.response.use(
     }
   },
   error => {
-    Message.error(error.message)// 提示错误消息
+    // console.log(error)
+    // 当token不完整或者被修改后，退出登录，跳转到登陆页面
+    if (error.response && error.response.status === 401) {
+      store.dispatch('user/logout')
+      router.push('/login')
+      Message.error('token超时')
+    } else {
+      Message.error(error.message)// 提示错误消息
+    }
     // 返回执行错误 让当前的执行链跳出成功 直接进入 catch
     return Promise.reject(error)
   }
