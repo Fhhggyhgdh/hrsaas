2a86c454059ba8c11d7574668c2b730895d8a329
moxianyou <1872499195@qq.com>
2022-10-13 10:56:45 +0800

    完成员工详情

--- src/api/employees.js
+++ src/api/employees.js
@@ -53,3 +53,49 @@ export function importEmployee(data) {
     data
   })
 }
+
+/** *
+ *  读取用户详情的基础信息
+ * **/
+export function getEmployeesInfo(id) {
+  return request({
+    url: `/employees/${id}/personalInfo`
+  })
+}
+
+/**
+ * 保存员工信息
+ * @param {*} data
+ * @returns
+ */
+export function saveEmployeesInfo(data) {
+  return request({
+    url: `/employees/${data.userId}/personalInfo`,
+    method: 'put',
+    data
+  })
+}
+
+/**
+ * 获取员工岗位信息
+ * @param {*} data
+ * @returns
+ */
+export function getEmployeesJobInfo(id) {
+  return request({
+    url: `/employees/${id}/jobs`
+  })
+}
+
+/**
+ * 保存员工岗位信息
+ * @param {*} data
+ * @returns
+ */
+export function saveEmployeesJobInfo(data) {
+  return request({
+    url: `/employees/${data.userId}/jobs`,
+    method: 'put',
+    data
+  })
+}
